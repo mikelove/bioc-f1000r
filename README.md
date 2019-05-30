d0e3b81ef0f8b7fba361d689af664c6e53e45422
mikelove <michaelisaiahlove@gmail.com>
2019-05-30 13:47:50 -0700

    adding readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Data and code for workflows posted to Bioconductor Channel on F1000Research
+
+![](bioc-f1000r-1.png)
+
+Notes:
+
+* PDFs were captured on May 30, 2019
+* Page counts, author number reflects the latest PDF
+* To qualify as a "workflow", the PDF had to contain code chunks,
+  although these may not always be evaluated
+
+![](bioc-f1000r-2.png)
+![](bioc-f1000r-3.png)
+![](bioc-f1000r-4.png)
+![](bioc-f1000r-5.png)
+![](bioc-f1000r-6.png)
+![](bioc-f1000r-7.png)
--- bioc-plot.R
+++ bioc-plot.R
@@ -27,6 +27,7 @@ cols <- c("black",
           rgb2(213,94,0),
           rgb2(204,121,167))
 
+png(file="bioc-f1000r-1.png",width=1000,height=800,res=120)
 ggplot(dat, aes(date, number, col=topic, shape=index, group=number)) +
   geom_line(show.legend=FALSE) +
   geom_point(fill="white", size=4, stroke=1) + 
@@ -38,27 +39,35 @@ ggplot(dat, aes(date, number, col=topic, shape=index, group=number)) +
   theme_bw() +
   xlim(as.Date(c("2015-09-01","2019-06-01"))) +
   ggtitle("Workflows on F1000Research's Bioconductor Channel")
+dev.off()
 
+png(file="bioc-f1000r-2.png",width=800,height=800,res=120)
 dat %>%
   filter(vers==1) %>%
   ggplot(aes(topic, fill=topic)) +
   geom_bar() +
   scale_fill_manual(values=cols) + 
   theme_bw()
+dev.off()
 
+png(file="bioc-f1000r-3.png",width=800,height=800,res=120)
 dat %>%
   filter(vers==1) %>%
   ggplot(aes(nauth)) +
   geom_bar(binwidth=1) +
   theme_bw() + xlab("number of authors")
+dev.off()
 
+png(file="bioc-f1000r-4.png",width=800,height=800,res=120)
 dat %>%
   filter(vers==1) %>%
   ggplot(aes(nrev, fill=index)) + 
   geom_bar(position="dodge") +
   theme_bw() +
   ggtitle("More reviewers helps indexing on first submission")
+dev.off()
 
+png(file="bioc-f1000r-5.png",width=800,height=800,res=120)
 dat %>%
   group_by(doi) %>%
   filter(vers==max(vers)) %>%
@@ -66,14 +75,18 @@ dat %>%
   geom_bar(position="dodge") +
   theme_bw() +
   ggtitle("More reviewers helps indexing on last submission")
+dev.off()
 
+png(file="bioc-f1000r-6.png",width=800,height=800,res=120)
 dat %>%
   filter(vers==1) %>%
   ggplot(aes(pages, group=index, fill=index, col=index)) + 
   geom_histogram(binwidth=15, position="dodge") + 
   theme_bw() +
   ggtitle("More pages doesn't mean indexing on first submission")
+dev.off()
 
+png(file="bioc-f1000r-7.png",width=800,height=800,res=120)
 dat %>%
   group_by(doi) %>%
   filter(vers==max(vers)) %>%
@@ -81,3 +94,4 @@ dat %>%
   geom_histogram(binwidth=15, position="dodge") + 
   theme_bw() +
   ggtitle("More pages helps indexing on last submission")
+dev.off()
